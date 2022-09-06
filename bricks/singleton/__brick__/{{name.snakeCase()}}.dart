34dda438e0eda6a070dc1c26e873402cbf32412d
farmery <nwachiifeanyi3000@gmail.com>
2022-09-06 15:56:09 +0100

    added singleton brick and view brick

---
+++ bricks/singleton/__brick__/{{name.snakeCase()}}.dart
@@ -0,0 +1,7 @@
+class {{name.pascalCase()}} {
+   static final {{name.pascalCase()}} _instance = {{name.pascalCase()}}._();
+  {{name.pascalCase()}}._();
+  factory {{name.pascalCase()}}() {
+    return _instance;
+  }
+}
